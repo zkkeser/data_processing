095ea8f7c3de4d047efd3a71d28da955a33dcdc0
Kubilay Keser <zkkeser@gmail.com>
2015-09-24 14:31:52 +0200

    bla

--- Homework/Javascript/script.js
+++ Homework/Javascript/script.js
@@ -9,7 +9,6 @@
 	//Split rawdata on lines
 	var rawDataLines = [];
 	var rawDataLines = rawData.split(/\r\n|\n/);
-	console.log(rawDataLines.length)
 	//Deletes the last item from the array because this was an empty one.
 	rawDataLines.pop(); //http://www.w3schools.com/jsref/jsref_pop.asp
 
@@ -18,16 +17,18 @@
 	arrayY = []
 	for (i in rawDataLines){
 		//Splits the lines into two parts, the date and the temperature
-		var splittedLine = [];
-  		splittedLine = rawDataLines[i].split(',')
+		var splitLine = [];
+  		splitLine = rawDataLines[i].split(',')
 
-  		splittedLine[0] = splittedLine[0].trim(); //http://www.w3schools.com/jsref/jsref_trim_string.asp
-  		splittedLine[1] = splittedLine[1].trim();
-  		splittedLine[0] = new Date(splittedLine[0]);
-  		splittedLine[1] = parseInt(splittedLine[1]); //http://www.w3schools.com/jsref/jsref_parseint.asp
-
-  		arrayX.push(splittedLine[0]);
-  		arrayY.push(splittedLine[1]);
+  		splitLine[0] = splitLine[0].trim(); //http://www.w3schools.com/jsref/jsref_trim_string.asp
+  		splitLine[1] = splitLine[1].trim();
+  		splitLine[0] = new Date(splitLine[0]);
+  		splitLine[1] = parseInt(splitLine[1]); //http://www.w3schools.com/jsref/jsref_parseint.asp
+  		//Cchange temp from xxx to xx,x
+		splitLine[1] = splitLine[1]/10
+		//Append data to arrays
+  		arrayX.push(splitLine[0]);
+  		arrayY.push(splitLine[1]);
 
   	}
 
@@ -41,17 +42,21 @@
 }
 }
 	//Get highest and lowest values of the data
-	var minimumY = Math.min.apply( Math, arrayY );
-	var maximumY = Math.max.apply( Math, arrayY );
+	var minimumY = Math.min.apply(Math,arrayY);
+	var maximumY = Math.max.apply(Math,arrayY);
 
 
 	//Transform X and Y
-	var transformX = createTransform([0,366],[0,1280]);
+	var transformX = createTransform([0,365],[0,1280]);
 	var transformY = createTransform([maximumY,minimumY],[0,720]);
 
 	//
 	var paper = document.getElementById('canvas');
 	var drawing = paper.getContext('2d');
+
+	drawing.beginPath();
+
+	drawing.strokeStyle = '#000000';
 
 	//Draw the X axis
 	xaxisXleft = transformX(0);
@@ -60,7 +65,7 @@
 	xaxisYright = transformY(0);
 	drawing.moveTo(xaxisXleft,xaxisYleft);
 	drawing.lineTo(xaxisXright,xaxisYright);
-	drawing.stroke();
+	
 
 	//Draw the Y axis
 	yaxisXtop = 0;
@@ -71,6 +76,8 @@
 	drawing.lineTo(0,720);
 	drawing.stroke();
 
+	drawing.closePath()
+
 
 	//This function feeds the array to the draw function
 	function feed(arrayY){
@@ -80,6 +87,7 @@
 
 	//This function draws the line from point to point
 	function draw(beginX,beginY,endX,endY){
+		drawing.beginPath()
 		beginX= transformX(beginX);
 		beginY= transformY(beginY);
 		endX = transformX(endX);
@@ -89,9 +97,9 @@
 		drawing.lineTo(endX,endY);
 		drawing.strokeStyle = '#295BFF';
 		drawing.stroke();
+		drawing.closePath();
 	}
 
 
-
-
 	feed(arrayY)
+
